61574cb3a46e80ef3902c060ed6363fee1647dd5
992139419 <992139419@qq.com>
2017-07-26 21:24:12 +0800

    'PC商城'

--- js/goodTypeAndAds.js
+++ js/goodTypeAndAds.js
@@ -4,23 +4,23 @@
 $(function(){
 // 	var goodType=["家用电器","运营商",""]
 // 	$(".goodType")
-var scrollAds = ["images/ads/ads-1.jpg","images/ads/ads-2.jpg","images/ads/ads-3.jpg","images/ads/ads-4.jpg","images/ads/ads-5.jpg","images/ads/ads-6.jpg",]
-var adsBottom =["images/ads/ads-bottom-1.jpg","images/ads/ads-bottom-2.png","images/ads/ads-bottom-3.png"];
-	$.each(scrollAds,function(index, val) {
-		// var img = $('<img src='+val+'>');
-		// var li = $('<li></li>');
-		// li.append(img);
-		// $('#ads .scroll-ads ul').append(li);
+    var scrollAds = ["images/ads/ads-1.jpg","images/ads/ads-2.jpg","images/ads/ads-3.jpg","images/ads/ads-4.jpg","images/ads/ads-5.jpg","images/ads/ads-6.jpg",]
+    var adsBottom =["images/ads/ads-bottom-1.jpg","images/ads/ads-bottom-2.png","images/ads/ads-bottom-3.png"];
+    $.each(scrollAds,function(index, val) {
+        // var img = $('<img src='+val+'>');
+        // var li = $('<li></li>');
+        // li.append(img);
+        // $('#ads .scroll-ads ul').append(li);
 
         // $('#ads .scroll-ads ul img')[index].src = val;
-        $('#carousel-example-generic img')[index].src = val;
+        $('#carousel-example-generic .carousel-inner img').eq(index).attr("src",val);
     });
     $.each(adsBottom,function(index, val) {
         // var img = $('<img src='+val+'>');
         // var li = $('<li></li>');
         // li.append(img);
         // $('#ads .ads-bottom ul').append(li);
-        $('#ads .ads-bottom ul img')[index].src = val;
+        $('#ads .ads-bottom ul img').eq(index).attr("src",val);
     });
     // ads触摸
     $('#carousel-example-generic').on('mouseover',function(){
@@ -32,9 +32,9 @@ var adsBottom =["images/ads/ads-bottom-1.jpg","images/ads/ads-bottom-2.png","ima
         // $('#carousel-example-generic .left').hide();
         // $('#carousel-example-generic .right').hide();
     });
-    // 右侧图片及服务 
-    $('.ads-right img')[0].src = './images/ads/ads-right.jpg';
-    $('.ads-right img').eq(0).css({'width':'100%','height':310});
+    // 右侧图片及服务
+    $('.ads-right .right-big-img img').eq(0).attr("src","./images/ads/ads-right.jpg");
+    $('.ads-right .right-big-img img').eq(0).css({'width':'100%','height':310});
     var txt=["话费","机票","酒店","游戏","企业购","加油卡","电影票","火车票","众筹","理财","礼品卡","白条"];
     for (var i = 0; i < txt.length; i++) {
         var a1 = $('<a href=""></a>');
@@ -44,7 +44,7 @@ var adsBottom =["images/ads/ads-bottom-1.jpg","images/ads/ads-bottom-2.png","ima
         li.append(a2);
         var icons = "./images/ads/sprite_fs@1x.png";
 
-        a1.css({"background-image":" url("+ icons +")"});
+        a1.css({"background-image":" url('"+ icons +"')"});
         if (i<4) {
             a1.css({"background-position":(-i*44)+"px 0px"});
         }else if(i>=4&&i<8){
